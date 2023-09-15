6d379d0eb5972da7c23e4a659316e911046c84e8
JRueda-277 <j.ruedavargas21@gmail.com>
2023-09-15 13:29:04 -0400

    General set up

---
+++ script.js
@@ -0,0 +1,24 @@
+const board = document.querySelector('#board');
+let rows = document.getElementsByClassName('gridRow');
+let columns = document.getElementsByClassName('gridColumn');
+
+function makeGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+function makeRows(numRows) {
+    for (let i = 0; i < numRows; i++) {
+        let row = document.createElement('div');
+        board.appendChild(row).className = 'gridRow';
+    }
+}
+
+function makeColumns(numColumns) {
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = 0; j < numColumns; j++) {
+            let column = document.createElement('div');
+            rows[j].appendChild(column).className = 'gridColumn';
+        }
+    }
+}
